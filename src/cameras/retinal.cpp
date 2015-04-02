7c2f94e9d8dca4be829ae90164ea9820b73fc495
Jonathan Kylstra <jskylstr@cs.unc.edu>
2015-04-02 11:27:18 -0400

    Added the scene files that definitely work.
    Updated the retina.cpp file

--- src/cameras/retinal.cpp
+++ src/cameras/retinal.cpp
@@ -19,7 +19,7 @@ RetinalCamera::RetinalCamera(const AnimatedTransform &cam2world, const float scr
     numCones    = 131072;
     xResolution = 512;
     yResolution = 256;
-    NodalPoint  = nodalP;
+    NodalPoint  = Point(256.0, 128.0, 0.0);
     PupilRadius = pupilradius;
 
     // set the z values and normals of the retina
@@ -58,7 +58,26 @@ float RetinalCamera::GenerateRay( const CameraSample &sample, Ray * ray) const
     Vector Cdir;
     RasterToCamera(Rdir, &Cdir);
 
-    *ray = RayDifferential(Pcamera, Normalize(Cdir), 0., INFINITY);
+    *ray = Ray(Pcamera, Normalize(Cdir), 0., INFINITY);
+
+    if(PupilRadius > 0.f)
+    {
+        // compute offset
+        float lensU, lensV;
+        ConcentricSampleDisk(sample.lensU, sample.lensV, &lensU, &lensV);
+        lensU *= PupilRadius;
+        lensV *= PupilRadius;
+
+        // compute new ray
+        Point Offset( 256.0 + lensU, 128.0 + lensV, 0.0);
+        Vector ODir( Offset - Pras );
+        Vector OCdir;
+        RasterToCamera( ODir, &OCdir );
+
+        ray->d = Normalize( OCdir );
+
+    }
+    
 	// This produces a vector from the retina to the nodal point
 	// All points that pass through the nodal point are unaltered
 	// need to alter this to include pupilradius information
@@ -76,6 +95,7 @@ float RetinalCamera::GenerateRayDifferential(const CameraSample &sample,
     int col = int( sample.imageX );
     float z = zValues[ row * xResolution + col ];
 
+    // convert from Raster to camera space
     Point Pras(sample.imageX, sample.imageY, z);
     Point Pcamera;
     RasterToCamera(Pras, &Pcamera);
@@ -84,17 +104,43 @@ float RetinalCamera::GenerateRayDifferential(const CameraSample &sample,
     Vector Cdir;
     RasterToCamera(Rdir, &Cdir);
 
-    *ray = RayDifferential(Pcamera, Normalize(Cdir), 0., INFINITY);
+    // Generate ray differential
+    *ray = RayDifferential( Pcamera, Normalize(Cdir), 0., INFINITY );
 
     ray->time = sample.time;
 
     CameraSample sshift = sample;
 
+
     Point rxNew(++sshift.imageX, sshift.imageY, zValues[ row * xResolution + col + 1 ] );
     Point ryNew(--sshift.imageX, ++sshift.imageY, zValues[ (row + 1) * xResolution + col ] );
-    Vector rxDir = NodalPoint - rxNew;
-    Vector ryDir = NodalPoint - ryNew;
+
+    Vector rxDir, ryDir;
+
+    if(PupilRadius > 0)
+    {
+        float lensU, lensV;
+        ConcentricSampleDisk(sample.lensU, sample.lensV, &lensU, &lensV);
+        lensU *= PupilRadius;
+        lensV *= PupilRadius;
+
+        Point NodalOffset(256.0 + lensU, 128.0 + lensV, 0.0);
+        Vector Odir( NodalOffset - Pras );
+        Vector OCdir;
+        RasterToCamera( Odir, &OCdir );
+        *ray = RayDifferential( Pcamera, Normalize(OCdir), 0., INFINITY );
+
+        rxDir = NodalOffset - rxNew;
+        ryDir = NodalOffset - ryNew;
+    }
+    else
+    {
+        rxDir = NodalPoint - rxNew;
+        ryDir = NodalPoint - ryNew;
+    }
     
+    
+
     RasterToCamera(rxNew, &(ray->rxOrigin) );
     RasterToCamera(ryNew, &(ray->ryOrigin) );
     RasterToCamera(rxDir, &(ray->rxDirection) );
@@ -148,7 +194,6 @@ RetinalCamera* CreateRetinalCamera(const ParamSet &params, const AnimatedTransfo
     {
         memcpy( screen, sw, 4*sizeof(float) );
     }
-
     
     return new RetinalCamera(cam2world, screen, shutteropen, shutterclose,
         film, pupilradius, focaldistance, nodalpoint, zVals, Normals);
